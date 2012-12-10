a9ef53c0cca20bbcbd58ef6d8ee380d49c812a4c
Serafim Panov <serafimpanov@gmail.com>
2012-12-10 13:47:32 +0600

    fix 12/10/12

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php  // $Id:,v 2.0 2012/05/20 16:10:00 Serafim Panov
 
-    $module->version   = 2012010702;  
+    $module->version   = 2012120702;  
     $module->cron      = 3600;  
     $module->requires  = 2011120500;
     $module->component = 'mod_reader';
